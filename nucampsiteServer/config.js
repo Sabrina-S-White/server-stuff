86ea7528d87c6e206734ef646d9675697e7741c5
Sabrina White <107942110+Sabrina-S-White@users.noreply.github.com>
2023-03-10 16:07:25 -0700

    Delete config.js

--- nucampsiteServer/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'secretKey': '12345-67890-09876-54321',
-  'mongoUrl' : 'mongodb://127.0.0.1:27017/nucampsite',
-  'facebook': {
-    clientId: '566344775458069',
-    clientSecret: '52d819a7cf1894f219afe2ffc92d0315'
-  }
-}
